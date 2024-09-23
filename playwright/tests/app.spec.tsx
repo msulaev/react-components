d95fb13760011da826e2d6a193341bba46d9cdf4
Mikhail Sulaev <49402569+msulaev@users.noreply.github.com>
2024-09-23 11:50:35 +0300

    Delete test.only

--- playwright/tests/app.spec.tsx
+++ playwright/tests/app.spec.tsx
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/experimental-ct-react";
 import App from "../../src/App";
 
 test.describe("App", () => {
-  test.only("should render and click", async ({ mount }) => {
+  test("should render and click", async ({ mount }) => {
     const component = await mount(<App />);
     await expect(component).toBeVisible();
     await component.locator("button").nth(0).click();
